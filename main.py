38bb0946c80002f403dd19764b783e48f5bc52a5
Pedro Henrique <pedroxlbd@gmail.com>
2024-01-05 23:51:29 -0300

    adding graph featuer

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 class GDPAnalysis:
     def __init__(self):
@@ -12,25 +15,51 @@ class GDPAnalysis:
 
         return f'{growth_rate * 10}%'
     
+    
     def getGrowthPerDecade(self, country: str, decade: int):
-        pass
+        get_country = self.data[self.data['Country Name'] == country]
+        decade1 = get_country[get_country['Year'] == decade]
+        if decade == 2010:
+            decade2 = get_country[get_country['Year'] == decade + 5]
+        else:
+            decade2 = get_country[get_country['Year'] == decade + 9]
+        growth_rate = round((decade2['Value'].values[0] / decade1['Value'].values[0]) - 1, 2)
+        
+        return f'{growth_rate * 10}%'
+    
+    
+    def GeneratePieChart(self, year: int):
+        val = self.data[self.data['Year'] == year]
+        
         
     def __repr__(self):
         return self.data.to_string()
+
 
 def main():
     gdp_analysis = GDPAnalysis()
     country = str(input('Which country would like to see the GDP analysis? '))
     ask = str(input('Would you like to see the yearly analysis or the decade analysis, or maybe both? ')).lower()
+    
     if ask in ['yearly']:
         year = int(input('From which year? '))
         print(gdp_analysis.getGrowthPerYear(country, year))
+        val = str(input("Would you like to see a graph representing the country's gdp compared to the world?"))
+        if val in ['yes']:
+            return gdp_analysis.GeneratePieChart()
+        
     if ask in ['decade']:
-        print('not implemented yet')
+        decade = int(input('From which decade? '))
+        print(gdp_analysis.getGrowthPerDecade(country, decade))
+        
     elif ask in ['both']:
-        print('not implemented yet')
+        year = int(input('From which year? '))
+        decade = int(input('From which decade? '))
+        print(gdp_analysis.getGrowthPerYear(country, year))
+        print(gdp_analysis.getGrowthPerDecade(country, decade))
     
-    print(gdp_analysis)
+    else:
+        print(gdp_analysis)
 
 if __name__ == '__main__':
      main()
